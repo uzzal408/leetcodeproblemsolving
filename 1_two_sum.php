948fd9dd348efa90ae9ca5c9ca41246813c5ab3e
uzzal408 <imail2226@diu.edu.bd>
2022-09-18 13:49:04 +0600

    updated

--- 1_two_sum.php
+++ 1_two_sum.php
@@ -1,15 +1,17 @@
 <?php
 class Solution{
    public function twoSum($nums, $target){
-        for ($i=0;$i<=count($nums);$i++){
-            if($nums[$i]+ $nums[$i+1]==$target){
-                return [$i,$i+1];
+        for ($i=0;$i<=count($nums)-1;$i++){
+            for ($j=1; $j<=count($nums); $j++)
+            if($nums[$i]+ $nums[$j]==$target){
+                return [$i,$j];
             }
         }
+        return [];
     }
 }
 
 $obj = new Solution();
-$nums = [3,2,4];
+$nums = [3,4,3];
 $result = $obj->twoSum($nums,6);
 print_r($result);
